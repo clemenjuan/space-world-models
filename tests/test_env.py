a85a9c0d102c0d8e3da8e5eead5a1703b895d177
Clemente <clementejuanoliver@gmail.com>
2026-06-17 11:30:47 +0200

    feat: idempotent Orekit JVM/data bootstrap

---
+++ tests/test_env.py
@@ -0,0 +1,9 @@
+import math
+
+
+def test_orekit_bootstrap_idempotent():
+    from envs.orekit_setup import ensure_orekit
+    ensure_orekit()
+    ensure_orekit()  # second call must be a no-op, not re-init the JVM
+    from org.orekit.utils import Constants
+    assert abs(Constants.EIGEN5C_EARTH_MU - 3.986004415e14) < 1e6
